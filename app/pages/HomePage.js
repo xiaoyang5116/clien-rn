021c5b0dc63625a699ffdc85b1d56fcfaaff54cc
KK <kk@codesky.com>
2022-02-25 01:17:54 +0800

    集成 Antd-Mobile-RN

--- app/pages/HomePage.js
+++ app/pages/HomePage.js
@@ -7,13 +7,15 @@ import {
   Component
 } from "../constants";
 
- import {
-   Button,
-   Text,
-   View,
- } from 'react-native';
+import {
+  Button,
+  Text,
+  View,
+} from 'react-native';
 
- class HomePage extends Component {
+import { List } from '@ant-design/react-native';
+
+class HomePage extends Component {
   
   onLogin = () => {
     this.props.dispatch(createAction('AppModel/login')())
@@ -21,10 +23,19 @@ import {
 
   render() {
     return (
-      <View>
-        <Text>你好:{this.props.name}</Text>
-        <Text>哈哈,如此优秀的我3~{this.props.age}</Text>
-        <Button title="点我" onPress={this.onLogin}/>
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 1, justifyContent:'flex-end' }}>
+          <List header='基础用法'>
+            <List.Item>1</List.Item>
+            <List.Item>2</List.Item>
+            <List.Item>3</List.Item>
+          </List>
+        </View>
+        <View style={{ flex: 1 }}>
+          <Text>你好:{this.props.name}</Text>
+          <Text>哈哈,如此优秀的我3~{this.props.age}</Text>
+          <Button title="点我" onPress={this.onLogin}/>
+        </View>
       </View>
     );
   }
