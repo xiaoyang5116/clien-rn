3f16eeafc8aba5eb3d02dff327a44491bd004238
yx <1443584075@qq.com>
2022-04-11 17:02:35 +0800

    删除安卓文件中的config文件

--- app/services/GetArticleDataApi.js
+++ app/services/GetArticleDataApi.js
@@ -2,123 +2,63 @@
 import yaml from 'js-yaml';
 import lo from 'lodash';
 import { errorMessage } from '../constants';
-import fs from "react-native-fs";
 
 let UNIQUE_KEY = 1;
 
 export async function GetArticleDataApi(id, path) {
+    let url = `http://localhost:8081/config/${id}/TXT/${id}_${path}.txt`;
+    return fetch(url)
+    .then(r => r.text(url))
+    .then(text => {
+        // 返回格式化的段落数据
+        const sectionData = [];
 
-    let url = `config/${id}/TXT/${id}_${path}.txt`;
+        const items = text.split(/#(START|END)[=]{1,}.*?[=]{1,}[\r\n]*/);
 
-    // 安卓打包后读取本地文件
-    return fs.readFileAssets(url, "utf8")
-        .then(text => {
-            // 返回格式化的段落数据
-            const sectionData = [];
+        let begin = false;
+        let code = '';
+        let key = 0;
+        items.forEach(e => {
+            if (lo.isEmpty(e))
+                return;
 
-            const items = text.split(/#(START|END)[=]{1,}.*?[=]{1,}[\r\n]*/);
-
-            let begin = false;
-            let code = '';
-            let key = 0;
-            items.forEach(e => {
-                if (lo.isEmpty(e))
-                    return;
-
-                if (lo.isEqual(e, 'START')) {
-                    begin = true;
-                } else if (lo.isEqual(e, 'END')) {
-                    try {
-                        const obj = yaml.load(code);
-                        if (lo.isArray(obj)) {
-                            obj.forEach(v => {
-                                sectionData.push({ key: UNIQUE_KEY++, type: 'code', object: v });
-                            });
-                        } else {
-                            sectionData.push({ key: UNIQUE_KEY++, type: 'code', object: obj });
-                        }
-                        begin = false;
-                        code = '';
-                    } catch (except) {
-                        errorMessage(`Yaml配置异常, 请检查文章指令区域语法. (${except.reason})`);
+            if (lo.isEqual(e, 'START')) {
+                begin = true;
+            } else if (lo.isEqual(e, 'END')) {
+                try {
+                    const obj = yaml.load(code);
+                    if (lo.isArray(obj)) {
+                        obj.forEach(v => {
+                            sectionData.push({ key: UNIQUE_KEY++, type: 'code', object: v });
+                        });
+                    } else {
+                        sectionData.push({ key: UNIQUE_KEY++, type: 'code', object: obj });
                     }
-                } else if (begin) {
-                    code = e;
-                } else {
-                    // 长文本拆分成若干段小文本
-                    let i = 0;
-                    const splits = e.split(/(.*?[\r\n]+)/).filter(e => !lo.isEmpty(e));
-                    while (i < splits.length) {
-                        // N行为一个小块
-                        const lines = lo.slice(splits, i, i + 10);
-
-                        // 去掉尾行换行符
-                        const last = lo.last(lines);
-                        lines[lines.length - 1] = last.substring(0, last.length - 1);
-
-                        i += lines.length;
-                        sectionData.push({ key: UNIQUE_KEY++, type: 'plain', content: lines.join('') });
-                    }
+                    begin = false;
+                    code = '';
+                } catch (except) {
+                    errorMessage(`Yaml配置异常, 请检查文章指令区域语法. (${except.reason})`);
                 }
-            });
+            } else if (begin) {
+                code = e;
+            } else {
+                // 长文本拆分成若干段小文本
+                let i = 0;
+                const splits = e.split(/(.*?[\r\n]+)/).filter(e => !lo.isEmpty(e));
+                while (i < splits.length) {
+                  // N行为一个小块
+                  const lines = lo.slice(splits, i, i + 10);
 
-            return sectionData;
-        })
-        .catch(console.error)
+                  // 去掉尾行换行符
+                  const last = lo.last(lines);
+                  lines[lines.length - 1] = last.substring(0, last.length - 1);
 
-    // let url = `http://localhost:8081/config/${id}/TXT/${id}_${path}.txt`;
-    // return fetch(url)
-    // .then(r => r.text(url))
-    // .then(text => {
-    //     // 返回格式化的段落数据
-    //     const sectionData = [];
+                  i += lines.length;
+                  sectionData.push({ key: UNIQUE_KEY++, type: 'plain', content: lines.join('') });
+                }
+            }
+        });
 
-    //     const items = text.split(/#(START|END)[=]{1,}.*?[=]{1,}[\r\n]*/);
-
-    //     let begin = false;
-    //     let code = '';
-    //     let key = 0;
-    //     items.forEach(e => {
-    //         if (lo.isEmpty(e))
-    //             return;
-
-    //         if (lo.isEqual(e, 'START')) {
-    //             begin = true;
-    //         } else if (lo.isEqual(e, 'END')) {
-    //             try {
-    //                 const obj = yaml.load(code);
-    //                 if (lo.isArray(obj)) {
-    //                     obj.forEach(v => {
-    //                         sectionData.push({ key: UNIQUE_KEY++, type: 'code', object: v });
-    //                     });
-    //                 } else {
-    //                     sectionData.push({ key: UNIQUE_KEY++, type: 'code', object: obj });
-    //                 }
-    //                 begin = false;
-    //                 code = '';
-    //             } catch (except) {
-    //                 errorMessage(`Yaml配置异常, 请检查文章指令区域语法. (${except.reason})`);
-    //             }
-    //         } else if (begin) {
-    //             code = e;
-    //         } else {
-    //             // 长文本拆分成若干段小文本
-    //             let i = 0;
-    //             const splits = e.split(/(.*?[\r\n]+)/).filter(e => !lo.isEmpty(e));
-    //             while (i < splits.length) {
-    //               // N行为一个小块
-    //               const lines = lo.slice(splits, i, i + 10);
-
-    //               // 去掉尾行换行符
-    //               const last = lo.last(lines);
-    //               lines[lines.length - 1] = last.substring(0, last.length - 1);
-
-    //               i += lines.length;
-    //               sectionData.push({ key: UNIQUE_KEY++, type: 'plain', content: lines.join('') });
-    //             }
-    //         }
-    //     });
-
-    //     return sectionData;
-    // });
+        return sectionData;
+    });
 }
--- app/services/GetFigureDataApi.js
+++ app/services/GetFigureDataApi.js
@@ -1,21 +1,11 @@
 import yaml from 'js-yaml';
-import fs from "react-native-fs";
 
 export async function GetFigureDataApi() {
-
-    // 安卓打包后读取本地文件
-    return fs.readFileAssets("config/TEST/figure.yml", "utf8")
-        .then(r => {
-            let data = yaml.load(r);
+    let url = "http://localhost:8081/config/TEST/figure.yml"
+    return fetch(url)
+        .then(r => r.text(url))
+        .then(text => {
+            let data = yaml.load(text);
             return data;
-        })
-        .catch(console.error)
-
-    // let url = "http://localhost:8081/config/TEST/figure.yml"
-    // return fetch(url)
-    //     .then(r => r.text(url))
-    //     .then(text => {
-    //         let data = yaml.load(text);
-    //         return data;
-    //     });
+        });
 }
