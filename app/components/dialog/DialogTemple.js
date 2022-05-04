718f0bf6e711cc0c241fb02271edaf7ade5cab11
yx <1443584075@qq.com>
2022-05-04 17:57:55 +0800

    单人对话框拆分

--- app/components/dialog/DialogTemple.js
+++ app/components/dialog/DialogTemple.js
@@ -3,14 +3,20 @@ import React from 'react'
 
 import HalfScreenDialog from './HalfScreenDialog';
 import FullScreenDialog from './FullScreenDialog';
+import SingleDialog from './singleDialog';
+import GameOverModal from '../modal/GameOverModal';
+import MultiplayerDialog from './MultiplayerDialog';
 
 export default function DialogTemple(props) {
-    switch (props.type) {
-        case 'HalfScreen':
-            return <HalfScreenDialog textAnimationType={props.textAnimationType} currentStyles={props.currentStyles} onDialogCancel={props.onDialogCancel} title={props.title} popUpComplex={props.popUpComplex} />
-        case 'FullScreen':
-            return <FullScreenDialog textAnimationType={props.textAnimationType} currentStyles={props.currentStyles} onDialogCancel={props.onDialogCancel} title={props.title} popUpComplex={props.popUpComplex} />
-        default:
-            return <HalfScreenDialog textAnimationType={props.textAnimationType} currentStyles={props.currentStyles} onDialogCancel={props.onDialogCancel} title={props.title} popUpComplex={props.popUpComplex} />
+    // console.log("props", props);
+    switch (props.style) {
+        case 6:
+            return <SingleDialog {...props} />
+        case 7:
+            return <GameOverDialog {...props} />
+        case 8:
+            return <MultiplayerDialog {...props} />
+        // default:
+        //     return <HalfScreenDialog {...props} />
     }
 }
---
+++ app/components/dialog/singleDialog/index.js
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+
+import { View, Text, TouchableWithoutFeedback, FlatList } from 'react-native';
+import { AppDispath, ThemeContext } from '../../../constants';
+
+import { TextButton } from '../../../constants/custom-ui';
+import TextAnimation from '../../textAnimation';
+
+import FullSingle from './FullSingle';
+import HalfSingle from './HalfSingle';
+
+const SingleDialog = props => {
+    const theme = React.useContext(ThemeContext);
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentTextList, setShowList] = useState(
+        props.popUpComplex[0].content,
+    );
+    const [showBtnList, setShowBtnList] = useState(props.popUpComplex[0].btn);
+
+    let currentDialogueLength = currentTextList.length - 1;
+
+    const nextParagraph = () => {
+        if (currentIndex < currentDialogueLength) {
+            setCurrentIndex(currentIndex + 1);
+        }
+    };
+    const nextDialogue = item => {
+        const newDialogue = props.popUpComplex.filter(i => i.key === item.tokey);
+        if (item.props !== undefined) {
+            AppDispath({
+                type: 'PropsModel/sendPropsBatch',
+                payload: { props: item.props },
+            });
+        }
+        if (newDialogue.length > 0) {
+            setShowList(newDialogue[0].content);
+            setShowBtnList(newDialogue[0].btn);
+            setCurrentIndex(0);
+        } else {
+            props.onDialogCancel();
+        }
+    };
+
+    const renderText = ({ item, index }) => {
+        if (index <= currentIndex) {
+            return (
+                <View style={{ marginTop: 12 }}>
+                    <TextAnimation
+                        icon={
+                            currentIndex === index && currentIndex < currentDialogueLength
+                                ? '▼'
+                                : ''
+                        }
+                        fontSize={20}
+                        type={props.textAnimationType}
+                        style={theme.contentColor3}>
+                        {item}
+                    </TextAnimation>
+                </View>
+            );
+        }
+        return null;
+    };
+    const renderBtn = ({ item }) => {
+        if (currentIndex === currentDialogueLength) {
+            return (
+                <View style={{ marginTop: 8 }}>
+                    <TextButton
+                        currentStyles={props.currentStyles}
+                        title={item.title}
+                        onPress={() => {
+                            nextDialogue(item);
+                        }}
+                    />
+                </View>
+            );
+        }
+        return null;
+    };
+
+    if (props.type === 'HalfScreen') {
+        return (
+            <HalfSingle
+                title={props.title}
+                nextParagraph={nextParagraph}
+                currentTextList={currentTextList}
+                renderText={renderText}
+                showBtnList={showBtnList}
+                renderBtn={renderBtn}
+            />
+        );
+    }
+    if (props.type === 'FullScreen') {
+        return (
+            <FullSingle
+                title={props.title}
+                nextParagraph={nextParagraph}
+                currentTextList={currentTextList}
+                renderText={renderText}
+                showBtnList={showBtnList}
+                renderBtn={renderBtn}
+            />
+        );
+    }
+    return null;
+};
+
+export default SingleDialog;
