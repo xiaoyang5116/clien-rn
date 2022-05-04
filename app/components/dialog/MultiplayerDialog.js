bbe4be0c70e36bedf8ad993715e3646b355e0a36
yx <1443584075@qq.com>
2022-05-04 10:12:24 +0800

    多人对话框增加发道具功能

--- app/components/dialog/MultiplayerDialog.js
+++ app/components/dialog/MultiplayerDialog.js
@@ -11,6 +11,7 @@ import {
 import {
     action,
     connect,
+    AppDispath,
     ThemeContext,
 } from "../../constants";
 import { changeAvatar } from '../../constants'
@@ -116,8 +117,14 @@ const MultiplayerDialog = (props) => {
     }
 
     //  点击按钮
-    const nextDialog = (tokey) => {
-        const newDialogData = viewData.sections.filter(s => s.key === tokey)
+    const nextDialog = (item) => {
+        const newDialogData = viewData.sections.filter(s => s.key === item.tokey)
+
+        // 发道具
+        if (item.props !== undefined) {
+            AppDispath({ type: 'PropsModel/sendPropsBatch', payload: { props: item.props } });
+        }
+
         if (newDialogData.length > 0) {
             addHistoryDialog({ id: newDialogData[0].dialog[0].id, content: newDialogData[0].dialog[0].content[0] })
             setCurrentDialogIndex(0)
@@ -175,7 +182,7 @@ const MultiplayerDialog = (props) => {
         if ((currentDialogIndex === currentDialogData.dialog.length - 1) && (currentContentIndex === currentContentLength)) {
             return (
                 <View style={{ marginTop: 12 }}>
-                    <TextButton title={item.title} onPress={() => { nextDialog(item.tokey) }} />
+                    <TextButton title={item.title} onPress={() => { nextDialog(item) }} />
                 </View>
             )
         }
