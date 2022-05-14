94df76ce2e189cf2c23fca7bcda05ba4f39c2ede
yx <1443584075@qq.com>
2022-05-14 09:59:24 +0800

    阅读器设置

--- app/components/readerSettings/customParagraph/index.js
+++ app/components/readerSettings/customParagraph/index.js
@@ -11,75 +11,70 @@ import {
 import { RButton } from '../_components';
 
 
-
-
 const CustomParagraph = (props) => {
     const theme = React.useContext(ThemeContext);
     const { readerStyle } = props
 
     const changeMatchColor = ({ color, bgColor }) => {
-        props.dispatch(action('ArticleModel/changeMatchColor')({ color: color, bgColor: bgColor }));
+        console.log('changeMatchColor', color, bgColor)
+        // props.dispatch(action('ArticleModel/changeMatchColor')({ color: color, bgColor: bgColor }));
     }
 
     const Wire = (props) => {
-        const Line = (props) => {
-            for (let index = 0; index < props.num; index++) {
-                return (
-                    <View style={{
-                        width: 20,
-                        height: 5,
-                        borderRadius: 2,
-                        marginTop: 12,
-                        backgroundColor: readerStyle.borderColor,
-                        overflow: 'hidden',
-                    }}></View>
-                )
-            }
+        let line = []
+        for (let index = 0; index < props.num; index++) {
+            line.push(<View key={index} style={[styles.wire, { backgroundColor: readerStyle.borderColor }]}></View>)
         }
         return (
-            <View>
-                <Line num={3} />
-            </View>
+            <TouchableOpacity
+                disabled={props.selected}
+                style={[theme.readerSettingRow_box]}
+                onPress={props.onPress}
+            >
+                <View style={[
+                    theme.readerSetting_border_2,
+                    {
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        borderColor: props.selected ? readerStyle.selectedBorderColor : readerStyle.borderColor,
+                    }
+                ]}>
+                    <View style={{
+                        flex: 1,
+                        transform: [{ scale: 0.5 }],
+                        justifyContent: 'space-evenly',
+                        alignItems: 'center',
+                    }}>
+                        {line}
+                    </View>
+                </View>
+            </TouchableOpacity>
         )
     }
 
     return (
         <View style={theme.readerSettingRow}>
             <View style={theme.readerSettingRow_left_item}>
-                <RButton
-                    color={readerStyle.matchColor1.bgColor}
-                    selected={(readerStyle.bgColor === readerStyle.matchColor1.bgColor)}
-                    onPress={() => { changeMatchColor(readerStyle.matchColor1) }}
-                />
+                <Wire num={4} />
+            </View>
+
+            <View style={theme.readerSettingRow_left_item}>
+                <Wire num={3} />
+            </View>
+
+            <View style={theme.readerSettingRow_left_item}>
+                <Wire num={2} />
             </View>
 
             <View style={theme.readerSettingRow_left_item}>
                 <RButton
-                    color={readerStyle.matchColor2.bgColor}
-                    selected={(readerStyle.bgColor === readerStyle.matchColor2.bgColor)}
-                    onPress={() => { changeMatchColor(readerStyle.matchColor2) }}
-                />
-            </View>
-
-            <View style={theme.readerSettingRow_left_item}>
-                <RButton
-                    color={readerStyle.matchColor3.bgColor}
-                    selected={(readerStyle.bgColor === readerStyle.matchColor3.bgColor)}
-                    onPress={() => { changeMatchColor(readerStyle.matchColor3) }}
-                />
-            </View>
-
-            <View style={theme.readerSettingRow_left_item}>
-                <RButton
-                    color={readerStyle.matchColor4.bgColor}
-                    selected={(readerStyle.bgColor === readerStyle.matchColor4.bgColor)}
-                    onPress={() => { changeMatchColor(readerStyle.matchColor4) }}
+                    title={"无"}
                 />
             </View>
 
             <View style={theme.readerSettingRow_right_item}>
                 <RButton
-                    title={"自定义配色"}
+                    title={"自定义排版"}
                     onPress={() => { }}
                 />
             </View>
@@ -89,3 +84,13 @@ const CustomParagraph = (props) => {
 }
 
 export default connect(state => ({ ...state.ArticleModel }))(CustomParagraph);
+
+
+const styles = StyleSheet.create({
+    wire: {
+        width: 30,
+        height: 5,
+        borderRadius: 2,
+        overflow: 'hidden',
+    }
+})
