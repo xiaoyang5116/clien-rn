b7e109697a1741edff4d1c680b0b61b9ed9ea3be
yx <1443584075@qq.com>
2022-05-20 18:11:10 +0800

    java代码禁用深色主题

--- android/app/src/main/java/com/wzproject/MainApplication.java
+++ android/app/src/main/java/com/wzproject/MainApplication.java
@@ -13,6 +13,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import androidx.appcompat.app.AppCompatDelegate;
 
 // import dk.madslee.imageCapInsets.RCTImageCapInsetPackage;
 
@@ -52,6 +53,7 @@ public class MainApplication extends Application implements ReactApplication {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
   }
 
   /**
