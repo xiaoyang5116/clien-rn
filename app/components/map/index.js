01a4ae22cb8e0f1162c52704fc19c43332cebf97
KK <kk@codesky.com>
2022-05-24 17:14:17 +0800

    完善城镇地图

--- app/components/map/index.js
+++ app/components/map/index.js
@@ -3,15 +3,19 @@ import FastImage from 'react-native-fast-image';
 
 import {
     View,
+    Text,
     FlatList,
+    TouchableWithoutFeedback,
 } from '../../constants/native-ui';
 
 import {
     Animated,
 } from 'react-native';
 
+import lo from 'lodash';
 import { px2pd } from '../../constants/resolution';
 import { getWindowSize } from '../../constants';
+import { confirm } from '../../components/dialog/ConfirmDialog';
 
 const MAP_DATA = [
     {
@@ -19,37 +23,77 @@ const MAP_DATA = [
         images: {
             front:  require('../../../assets/maps/town/front_01.png'),
             middle: require('../../../assets/maps/town/mid_01.png'),
-        }
+        },
+        farPoints: [
+            { style: { left: 150, top: 200 }, title: '桃花岛' },
+        ],
+        nearPoints: [
+            { style: { left: 80, top: 300 }, title: '桃花岛' },
+        ],
     },
     {
         id: 2,
         images: {
             front:  require('../../../assets/maps/town/front_02.png'),
             middle: require('../../../assets/maps/town/mid_02.png'),
-        }
+        },
+        farPoints: [
+            { style: { left: 150, top: 180 }, title: '桃花岛' },
+        ],
+        nearPoints: [
+        ],
     },
     {
         id: 3,
         images: {
             front:  require('../../../assets/maps/town/front_03.png'),
             middle: require('../../../assets/maps/town/mid_03.png'),
-        }
+        },
+        farPoints: [
+            { style: { left: 150, top: 200 }, title: '桃花岛' },
+        ],
+        nearPoints: [
+            { style: { left: 80, top: 300 }, title: '桃花岛' },
+        ],
     },
     {
         id: 4,
         images: {
             front:  require('../../../assets/maps/town/front_04.png'),
             middle: require('../../../assets/maps/town/mid_04.png'),
-        }
+        },
+        farPoints: [
+            { style: { left: 150, top: 180 }, title: '桃花岛' },
+        ],
+        nearPoints: [
+        ],
     },
     {
         id: 5,
         images: {
             front:  require('../../../assets/maps/town/front_05.png'),
             middle: require('../../../assets/maps/town/mid_05.png'),
-        }
+        },
+        farPoints: [
+        ],
+        nearPoints: [
+            { style: { left: 80, top: 300 }, title: '桃花岛' },
+        ],
     },
 ];
+
+const EntryButton = (props) => {
+    return (
+        <TouchableWithoutFeedback onPress={() => {
+                confirm('确认进入？');
+            }}>
+            <View style={{ ...props.style }}>
+                <FastImage style={{ width: px2pd(84), height: px2pd(211) }} source={require('../../../assets/themes/normal/button/town_button.png')} />
+                <Text style={{ position: 'absolute', left: 8, top: 12, width: 20, color: '#333' }}>{props.title}</Text>
+            </View>
+        </TouchableWithoutFeedback>
+    );
+}
 
 const MapView = (props) => {
 
@@ -61,14 +105,42 @@ const MapView = (props) => {
 
     const renderItem = (data) => {
         const { item } = data;
+
+        let idx = 0;
+        const farPoints = [];
+        if (lo.isArray(item.farPoints)) {
+            item.farPoints.forEach(e => {
+                farPoints.push(
+                    <View style={{ position: 'absolute' }} key={idx}>
+                        <EntryButton {...e} />
+                    </View>
+                );
+                idx++;
+            });
+        }
+
+        const nearPoints = [];
+        if (lo.isArray(item.nearPoints)) {
+            item.nearPoints.forEach(e => {
+                nearPoints.push(
+                    <Animated.View style={{ position: 'absolute', left: leftPos, top: fixedTop }} key={idx}>
+                        <EntryButton {...e} />
+                    </Animated.View>
+                );
+                idx++;
+            });
+        }
+
         return (
             <View style={{ width: 'auto', height: '100%', backgroundColor: '#fff' }}>
                 {/* 中景图 */}
                 <FastImage style={{ width: px2pd(500), height: px2pd(1300), top: fixedTop }} source={item.images.middle} />
                 {/* 前景图 */}
-                <Animated.View style={{ position: 'absolute', left: leftPos, top: fixedTop }}>
+                <Animated.View style={{ position: 'absolute', left: leftPos, top: fixedTop }} pointerEvents='none'>
                     <FastImage style={{ width: px2pd(500), height: px2pd(1300) }} source={item.images.front} />
                 </Animated.View>
+                {farPoints}
+                {nearPoints}
             </View>
         );
     }
