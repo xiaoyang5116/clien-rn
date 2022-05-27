5ccdcda9d4332b66b5217d0b560701d24078fc0e
KK <kk@codesky.com>
2022-05-27 20:02:58 +0800

    优化：场景地图

--- app/components/maps/SceneMap.js
+++ app/components/maps/SceneMap.js
@@ -13,13 +13,14 @@ import {
   PanResponder,
 } from 'react-native';
 
-import {
+import { 
   getWindowSize
 } from '../../constants';
 
 import lo from 'lodash';
 import FastImage from 'react-native-fast-image';
 import { px2pd } from '../../constants/resolution';
+import Easing from 'react-native/Libraries/Animated/Easing';
 
 const WIN_SIZE = getWindowSize();
 
@@ -33,6 +34,11 @@ const GRID_PX_WIDTH = px2pd(290);
 const GRID_PX_HEIGHT = px2pd(84);
 // 地图magin值
 const MAP_MARGIN_VALUE = 4;
+
+// 小地图尺寸
+const MAP_SMALL_SIZE = { width: px2pd(1064), height: px2pd(464) };
+// 大地图尺寸
+const MAP_BIG_SIZE = { width: px2pd(1064), height: px2pd(1664) };
 
 const LINES = [
   { direction: 1, style: { width: px2pd(142), height: px2pd(142) }, img: require('../../../assets/bg/map_line1.png') },
@@ -57,14 +63,18 @@ const getLineConfig = (p1, p2) => {
 const SceneMap = (props) => {
   // 初始化起始坐标
   const initialXY = {
-    x: ((props.mapSize.width - MAP_MARGIN_VALUE * 2) / 2) - (GRID_PX_WIDTH / 2) - (props.initialCenterPoint[0] * (GRID_PX_WIDTH + GRID_SPACE)),
-    y: ((props.mapSize.height - MAP_MARGIN_VALUE * 2) / 2) - (GRID_PX_HEIGHT / 2) + (props.initialCenterPoint[1] * (GRID_PX_HEIGHT + GRID_SPACE)),
+    x: ((MAP_SMALL_SIZE.width - MAP_MARGIN_VALUE * 2) / 2) - (GRID_PX_WIDTH / 2) - (props.initialCenterPoint[0] * (GRID_PX_WIDTH + GRID_SPACE)),
+    y: ((MAP_SMALL_SIZE.height - MAP_MARGIN_VALUE * 2) / 2) - (GRID_PX_HEIGHT / 2) + (props.initialCenterPoint[1] * (GRID_PX_HEIGHT + GRID_SPACE)),
   };
 
   // 地图移动坐标
   const mapPos = React.useRef(new Animated.ValueXY(initialXY)).current;
   // 各种状态数据
   const status = React.useRef({ prevX: 0, prevY: 0 }).current;
+  // 大地图坐标
+  const bigMapTransY = React.useRef(new Animated.Value(WIN_SIZE.height)).current;
+  // 大地图事件模型
+  const [bigPointerEvent, setBigPointerEvent] = React.useState('none');
 
   // 滑动处理器
   const panResponder = React.useRef(PanResponder.create({
@@ -75,12 +85,41 @@ const SceneMap = (props) => {
     },
     onPanResponderMove: (evt, gestureState) => {
       mapPos.setValue({ x: status.prevX + gestureState.dx, y: status.prevY + gestureState.dy });
+      console.debug('move');
     },
     onPanResponderRelease: (evt, gestureState) => {
       status.prevX = mapPos.x._value;
       status.prevY = mapPos.y._value;
     },
   })).current;
+
+  const maxMapHandler = (e) => {
+    Animated.timing(bigMapTransY, {
+      toValue: 0,
+      duration: 300,
+      easing: Easing.out(Easing.cubic),
+      useNativeDriver: false,
+    }).start((r) => {
+      const { finished } = r;
+      if (finished) {
+        setBigPointerEvent('auto');
+      }
+    });
+  }
+
+  const minMapHandler = (e) => {
+    Animated.timing(bigMapTransY, {
+      toValue: WIN_SIZE.height,
+      duration: 300,
+      easing: Easing.in(Easing.cubic),
+      useNativeDriver: false,
+    }).start((r) => {
+      const { finished } = r;
+      if (finished) {
+        setBigPointerEvent('none');
+      }
+    });
+  }
 
   // 渲染格子
   const grids = [];
@@ -119,19 +158,30 @@ const SceneMap = (props) => {
   });
 
   return (
-    // <View style={{ flex: 1, overflow: 'hidden' }} {...panResponder.panHandlers}>
-    //   <FastImage style={{ position: 'absolute', width: '100%', height: '100%' }} source={require('../../../assets/bg/scene_map.png')} />
-    //   <Animated.View style={{ position: 'absolute', transform: [{ translateX: mapPos.x }, { translateY: mapPos.y }] }}>
-    //     {lines}
-    //     {grids}
-    //   </Animated.View>
-    // </View>
-    <View style={{ flex: 1 }} {...panResponder.panHandlers}>
+    <View style={[{}, { ...MAP_SMALL_SIZE }]} {...panResponder.panHandlers}>
       <FastImage style={{ position: 'absolute', width: '100%', height: '100%' }} source={require('../../../assets/bg/scene_map.png')} />
+
+      {/* 小地图网格 */}
       <View style={{ flex: 1, margin: 4, overflow: 'hidden' }}>
         <Animated.View style={{ position: 'absolute', transform: [{ translateX: mapPos.x }, { translateY: mapPos.y }] }}>
           {lines}
           {grids}
+        </Animated.View>
+      </View>
+
+      {/* 最大化按钮 */}
+      <TouchableWithoutFeedback onPress={maxMapHandler}>
+        <FastImage style={{ position: 'absolute', right: 10, top: -6, width: px2pd(106), height: px2pd(46) }} source={require('../../../assets/button/map_maximize_button.png')} />
+      </TouchableWithoutFeedback>
+
+      {/* 大地图 */}
+      <View style={[{ position: 'absolute', bottom: 0, overflow: 'hidden' }, { width: MAP_BIG_SIZE.width, height: MAP_BIG_SIZE.height + 10 }]} pointerEvents={bigPointerEvent}>
+        <Animated.View style={[{ position: 'absolute', bottom: 0 }, { transform: [{ translateY: bigMapTransY }] }, { ...MAP_BIG_SIZE }]}>
+          <FastImage style={[{ width: '100%', height: '100%' }]} source={require('../../../assets/bg/scene_map_big.png')} />
+          {/* 缩小按钮 */}
+          <TouchableWithoutFeedback onPress={minMapHandler}>
+            <FastImage style={{ position: 'absolute', right: 10, top: -6, width: px2pd(106), height: px2pd(46) }} source={require('../../../assets/button/map_min_button.png')} />
+          </TouchableWithoutFeedback>
         </Animated.View>
       </View>
     </View>
@@ -143,15 +193,12 @@ const SceneMap = (props) => {
 SceneMap.propTypes = {
   // 地图数据
   data: PropTypes.array,
-  // 地图尺寸
-  mapSize: PropTypes.object,
   // 起始中心点
   initialCenterPoint: PropTypes.array,
 };
 
 SceneMap.defaultProps = {
   data: [],
-  mapSize: { width: WIN_SIZE.width, height: WIN_SIZE.height },
   initialCenterPoint: [0, 0],
 };
 
