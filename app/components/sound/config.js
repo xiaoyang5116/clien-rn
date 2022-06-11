0573bd00272bc2233ebd16f4729b40751b6f6869
lcy <545693471@qq.com>
2022-06-11 17:01:23 +0800

    音乐音效修改

--- app/components/sound/config.js
+++ app/components/sound/config.js
@@ -1,13 +1,57 @@
-// 音频资源
+// 音频资源注册
+// BGM=背景音乐 SE=音效。命名缩略文字，B战斗，C场地，Q气氛，R日常,T探索
 export const SOUND_RESOURCES = [
-    { id: 'BGM00001', source: require('../../../assets/sound/bg1.mp3') },
-    { id: 'BGM00002', source: require('../../../assets/sound/bg2.mp3') },
-    { id: 'BGM00003', source: require('../../../assets/sound/bg3.mp3') },
-    { id: 'BGM00004', source: require('../../../assets/sound/bg4.mp3') },
-    { id: 'BGM00005', source: require('../../../assets/sound/bg5.mp3') },
-    { id: 'BGM00006', source: require('../../../assets/sound/bg6.mp3') },
-    { id: 'BGM00007', source: require('../../../assets/sound/bg7.mp3') },
-    { id: 'BGM00008', source: require('../../../assets/sound/bg8.mp3') },
-    
-    { id: 'SE000001', source: require('../../../assets/sound/effect1.wav') },
+    // ================音乐===============================
+    { id: 'BGM_B_0002', source: require('../../../assets/sound/BGM/BGM_B_0002.mp3') },
+    { id: 'BGM_B_0003', source: require('../../../assets/sound/BGM/BGM_B_0003.mp3') },
+    { id: 'BGM_C_0001', source: require('../../../assets/sound/BGM/BGM_C_0001.mp3') },
+    { id: 'BGM_C_0002', source: require('../../../assets/sound/BGM/BGM_C_0002.mp3') },
+    { id: 'BGM_Q_0001', source: require('../../../assets/sound/BGM/BGM_Q_0001.mp3') },
+    { id: 'BGM_Q_0002', source: require('../../../assets/sound/BGM/BGM_Q_0002.mp3') },
+    { id: 'BGM_Q_0003', source: require('../../../assets/sound/BGM/BGM_Q_0003.mp3') },
+    { id: 'BGM_Q_0004', source: require('../../../assets/sound/BGM/BGM_Q_0004.mp3') },
+    { id: 'BGM_R_0001', source: require('../../../assets/sound/BGM/BGM_R_0001.mp3') },
+    { id: 'BGM_R_0002', source: require('../../../assets/sound/BGM/BGM_R_0002.mp3') },
+    { id: 'BGM_R_0003', source: require('../../../assets/sound/BGM/BGM_R_0003.mp3') },
+    { id: 'BGM_T_0001', source: require('../../../assets/sound/BGM/BGM_T_0001.mp3') },
+
+
+    // ================音效===============================
+    // SE=音效。命名缩略文字，KO战斗，CQ气氛 MS魔法释放 UE界面音效
+    { id: 'SE_CQ_0008', source: require('../../../assets/sound/SE/SE_CQ_0008.wav') },
+
+    { id: 'SE_ED_0021', source: require('../../../assets/sound/SE/SE_ED_0021.wav') },
+
+    { id: 'SE_KO_0001', source: require('../../../assets/sound/SE/SE_KO_0001.wav') },
+    { id: 'SE_KO_0002', source: require('../../../assets/sound/SE/SE_KO_0002.wav') },
+    { id: 'SE_KO_0011', source: require('../../../assets/sound/SE/SE_KO_0011.wav') },
+    { id: 'SE_KO_0014', source: require('../../../assets/sound/SE/SE_KO_0014.wav') },
+    { id: 'SE_KO_0015', source: require('../../../assets/sound/SE/SE_KO_0015.wav') },
+    { id: 'SE_KO_0021', source: require('../../../assets/sound/SE/SE_KO_0021.wav') },
+    { id: 'SE_KO_0022', source: require('../../../assets/sound/SE/SE_KO_0022.wav') },
+    { id: 'SE_KO_0023', source: require('../../../assets/sound/SE/SE_KO_0023.wav') },
+    { id: 'SE_KO_0024', source: require('../../../assets/sound/SE/SE_KO_0024.wav') },
+    { id: 'SE_KO_0025', source: require('../../../assets/sound/SE/SE_KO_0025.wav') },
+    { id: 'SE_KO_0030', source: require('../../../assets/sound/SE/SE_KO_0021.wav') },
+
+    { id: 'SE_MS_0001', source: require('../../../assets/sound/SE/SE_MS_0001.wav') },
+    { id: 'SE_MS_0002', source: require('../../../assets/sound/SE/SE_MS_0002.wav') },
+    { id: 'SE_MS_0003', source: require('../../../assets/sound/SE/SE_MS_0003.wav') },
+
+    { id: 'SE_MS_0031', source: require('../../../assets/sound/SE/SE_MS_0031.wav') },
+    { id: 'SE_MS_0041', source: require('../../../assets/sound/SE/SE_MS_0041.wav') },
+
+    { id: 'SE_UE_0001', source: require('../../../assets/sound/SE/SE_UE_0001.wav') },
+    { id: 'SE_UE_0002', source: require('../../../assets/sound/SE/SE_UE_0002.wav') },
+    { id: 'SE_UE_0003', source: require('../../../assets/sound/SE/SE_UE_0003.wav') },
+    { id: 'SE_UE_0005', source: require('../../../assets/sound/SE/SE_UE_0005.wav') }, 
+ 
+
+
+
+
+
+
+
+
 ];
