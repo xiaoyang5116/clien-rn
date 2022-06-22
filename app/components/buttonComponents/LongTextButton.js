ce8ca36dadb8922ac01833ba3c546404662760e6
yx <1443584075@qq.com>
2022-06-22 10:47:28 +0800

    长文字按钮优化

--- app/components/buttonComponents/LongTextButton.js
+++ app/components/buttonComponents/LongTextButton.js
@@ -1,4 +1,4 @@
-import { View, Text, TouchableHighlight, Image } from 'react-native';
+import { View, Text, TouchableHighlight, Image, StyleSheet } from 'react-native';
 import React from 'react';
 
 import PropTypes from 'prop-types';
@@ -7,7 +7,7 @@ import { ThemeContext } from '../../constants';
 import { px2pd } from '../../constants/resolution';
 
 export const LongTextButton = props => {
-    const { title, onPress, source, borderColor, color, fontSize } = props;
+    const { title, onPress, fontSize } = props;
 
     const theme = React.useContext(ThemeContext);
 
@@ -18,54 +18,30 @@ export const LongTextButton = props => {
     };
 
     return (
-        <View
-            style={{
-                justifyContent: 'center',
-                alignItems: 'center',
-                overflow: 'hidden',
-            }}>
+        <View style={styles.center}>
             <TouchableHighlight
                 underlayColor={'#fff'}
                 activeOpacity={0.4}
-                style={{
-                    width: px2pd(1016),
-                    height: px2pd(102),
-                    borderWidth: px2pd(3),
-                    borderRadius: 3,
-                    borderColor: borderColor ? borderColor : "#404040",
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    overflow: 'hidden',
-                    zIndex: 1,
-                }}
+                style={[
+                    styles.center,
+                    styles.btnContainer,
+                    { borderColor: '#404040' },
+                ]}
                 onPress={onPressHandler}
             >
                 <Image
-                    style={{ width: px2pd(1010), height: px2pd(96) }}
-                    source={source ? source : theme.LongTBBgImage}
+                    style={styles.imageContainer}
+                    source={theme.LongTBBgImage}
                 />
             </TouchableHighlight>
-            <View
-                pointerEvents="none"
-                style={{
-                    position: "absolute",
-                    zIndex: 2
-                }}>
-                <Image
-                    style={{
-                        width: px2pd(1010),
-                        height: px2pd(96),
-                    }}
-                    source={theme.LongTBBorderImage}
-                />
+            <View pointerEvents="none" style={styles.patternContainer}>
+                <Image style={styles.imageContainer} source={theme.LongTBBorderImage} />
             </View>
-
-            <View pointerEvents="none" style={{ position: 'absolute', zIndex: 3 }}>
-                <Text
-                    style={{
-                        fontSize: fontSize ? fontSize : 18,
-                        color: color ? color : theme.button.color,
-                    }}>
+            <View pointerEvents="none" style={styles.textContainer}>
+                <Text style={{
+                    fontSize: fontSize,
+                    color: theme.button.color,
+                }}>
                     {title}
                 </Text>
             </View>
@@ -73,11 +49,39 @@ export const LongTextButton = props => {
     );
 };
 
+const styles = StyleSheet.create({
+    center: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        overflow: 'hidden',
+    },
+    btnContainer: {
+        width: px2pd(1016),
+        height: px2pd(102),
+        borderWidth: px2pd(3),
+        borderRadius: 3,
+        zIndex: 1,
+    },
+    imageContainer: {
+        width: px2pd(1010),
+        height: px2pd(96),
+    },
+    patternContainer: {
+        position: 'absolute',
+        zIndex: 2,
+    },
+    textContainer: {
+        position: 'absolute',
+        zIndex: 3,
+    },
+});
+
 LongTextButton.propTypes = {
     title: PropTypes.string,
     onPress: PropTypes.func,
-    source: PropTypes.number,
-    borderColor: PropTypes.string,
-    color: PropTypes.string,
-    fontSize: PropTypes.number
+    fontSize: PropTypes.number,
+};
+
+LongTextButton.defaultProps = {
+    fontSize: 18
 }
