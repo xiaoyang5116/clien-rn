71fa35d87ec804e28772dbc0846789a23c4b84e5
KK <kk@codesky.com>
2022-07-04 15:58:16 +0800

    修复场景对话框按钮残影问题

--- app/components/modal/Narration.js
+++ app/components/modal/Narration.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { StyleSheet } from 'react-native';
+import { TextButton } from '../../constants/custom-ui';
 import { Button, Text, View, TouchableWithoutFeedback, } from '../../constants/native-ui';
 
 
 const BlackNarration = (props) => {
   const currentStyles = props.currentStyles;
+  const label = '>>>';
+
   return (
     <TouchableWithoutFeedback onPress={props.onAsideNext}>
       <View style={[currentStyles.asideCenter]}>
@@ -17,12 +21,27 @@ const BlackNarration = (props) => {
         </View>
         <View style={currentStyles.asideBottomContainer}>
           <View style={[currentStyles.asideBottomBanner, { backgroundColor: currentStyles.button.backgroundColor }]}>
-            <Button title='>>>' onPress={props.onAsideNext} color={currentStyles.button.color} />
+            {/* <Button title='@@@>>>' onPress={props.onAsideNext} color={currentStyles.button.color} /> */}
+            <TouchableWithoutFeedback onPress={props.onAsideNext}>
+              <View style={[styles.buttonView, { backgroundColor: currentStyles.button.color }]}><Text style={styles.buttonText}>{label}</Text></View>
+            </TouchableWithoutFeedback>
           </View>
         </View>
       </View>
     </TouchableWithoutFeedback>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonView: {
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    height: 35,
+  },
+
+  buttonText: {
+    color: '#fff',
+  },
+});
 
 export default BlackNarration;
