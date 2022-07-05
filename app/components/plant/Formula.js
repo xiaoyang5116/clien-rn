e398db207d9a8cf8d24a3d1842476a7d3dcbb04f
yx <1443584075@qq.com>
2022-07-05 14:45:54 +0800

    配方界面

--- app/components/plant/Formula.js
+++ app/components/plant/Formula.js
@@ -1,32 +1,65 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
-import RootView from '../RootView'
-import { HalfPanel } from '../panel/HalfPanel'
-import { TextButton } from '../../constants/custom-ui'
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    FlatList
+} from 'react-native';
+import React from 'react';
 
-const FormulaComponent = (props) => {
+import RootView from '../RootView';
+import { HalfPanel } from '../panel/HalfPanel';
+import { TextButton } from '../../constants/custom-ui';
+
+
+const FormulaComponent = props => {
     return (
-        <HalfPanel backgroundColor={"rgba(0,0,0,0.7)"}>
-            <View style={{ flex: 1, backgroundColor: "#fff" }}>
-                <Text style={{ height: 50, lineHeight: 50, textAlign: "center", fontSize: 24, color: "#000", backgroundColor: "#ccc" }}>请选择配方</Text>
-                <View style={{ flex: 1 }}></View>
-                <View style={{}}>
+        <HalfPanel backgroundColor={'rgba(0,0,0,0.7)'}>
+            <View style={{ flex: 1, backgroundColor: '#fff' }}>
+                <Text style={{
+                    height: 50,
+                    lineHeight: 50,
+                    textAlign: 'center',
+                    fontSize: 24,
+                    color: '#000',
+                    backgroundColor: '#ccc',
+                }}>
+                    种植选择
+                </Text>
+                <View style={{ flex: 1 }}>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-evenly' }}>
+                        <TextButton title={'等级'} />
+                        <TextButton title={'种植时间'} />
+                        <TextButton title={'品质'} />
+                    </View>
+                    <View>
+                        <FlatList
+
+                        />
+                    </View>
+                </View>
+                <View>
                     <TextButton
-                        title={"退出"}
-                        onPress={() => { props.onClose() }}
+                        title={'退出'}
+                        onPress={() => {
+                            props.onClose();
+                        }}
                     />
                 </View>
             </View>
         </HalfPanel>
-    )
-}
+    );
+};
 
-export default FormulaComponent
+export default FormulaComponent;
 
 export class Formula {
     static show() {
         const key = RootView.add(
-            <FormulaComponent onClose={() => { RootView.remove(key) }} />
-        )
+            <FormulaComponent
+                onClose={() => {
+                    RootView.remove(key);
+                }}
+            />,
+        );
     }
 }
