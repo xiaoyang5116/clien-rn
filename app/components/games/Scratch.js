4a987e3f1b2747676a1ded8d01bfae99b4b40b1a
yx <1443584075@qq.com>
2022-07-12 18:01:54 +0800

    刮刮乐图片设置

--- app/components/games/Scratch.js
+++ app/components/games/Scratch.js
@@ -5,6 +5,7 @@ import ScratchView from 'react-native-scratch'
 
 import { HalfPanel } from "../panel";
 import Toast from '../toast'
+import { TextButton } from '../../constants/custom-ui';
 
 class Scratch extends Component {
 
@@ -18,12 +19,13 @@ class Scratch extends Component {
 
     onScratchDone = ({ isScratchDone, id }) => {
         // Do something
+        // Toast.show("ssss")
     }
 
     onScratchTouchStateChanged = ({ id, touchState }) => {
         // Example: change a state value to stop a containing
         // FlatList from scrolling while scratching
-        this.setState({ scrollEnabled: !touchState });
+        // this.setState({ scrollEnabled: !touchState });
     }
     componentDidMount() {
         // const cardBg = require("../../../assets/bg/lottery_bg2.jpg")
@@ -34,18 +36,17 @@ class Scratch extends Component {
         // console.log("ssss", require("../../../assets/bg/lottery_bg2.jpg"));
         // const cardBg = require("../../../assets/bg/lottery_bg2.jpg")
         return (
-            <HalfPanel backgroundColor={"rgba(0,0,0,0.7)"}>
-                <View style={{ flex: 1, backgroundColor: "#fff", justifyContent: "center", alignItems: "center" }}>
-                    <Text style={{ fontSize: 20, color: "#000" }}>刮刮乐</Text>
-                    <View style={{ width: 300, height: 300 }}>
+            <HalfPanel backgroundColor={"rgba(0,0,0,0.7)"} height={"70%"}>
+                <View style={{ flex: 1, justifyContent: "space-around", alignItems: "center" }}>
+                    <Text style={{ fontSize: 24, color: "#ccc" }}>刮刮乐</Text>
+                    <View style={{ width: 300, height: 300, overflow: "hidden" }}>
                         <ScratchView
                             id={1} // ScratchView id (Optional)
                             brushSize={50} // Default is 10% of the smallest dimension (width/height)
-                            threshold={70} // Report full scratch after 70 percentage, change as you see fit. Default is 50
+                            threshold={45} // Report full scratch after 70 percentage, change as you see fit. Default is 50
                             fadeOut={true} // Disable the fade out animation when scratch is done. Default is true
                             placeholderColor="#AAAAAA" // Scratch color while image is loading (or while image not present)
-                            // imageUrl={Image.resolveAssetSource(cardBg).uri} // A url to your image (Optional)
-                            // imageUrl={uri:Image.resolveAssetSource(cardBg).name} // A url to your image (Optional)
+                            imageUrl={"https://s2.loli.net/2022/07/12/jr2siQNAknW3Fpz.jpg"} // A url to your image (Optional)
                             resourceName="your_image" // An image resource name (without the extension like '.png/jpg etc') in the native bundle of the app (drawble for Android, Images.xcassets in iOS) (Optional)
                             resizeMode="stretch" // Resize the image to fit or fill the scratch view. Default is stretch (cover|contain|stretch)
                             onImageLoadFinished={this.onImageLoadFinished} // Event to indicate that the image has done loading
@@ -53,7 +54,9 @@ class Scratch extends Component {
                             onScratchProgressChanged={this.onScratchProgressChanged} // Scratch progress event while scratching
                             onScratchDone={this.onScratchDone} // 完成时调用
                         />
+                        <Image style={{ width: "100%", height: "100%", zIndex: -1 }} source={require("../../../assets/avatar/1.jpg")} />
                     </View>
+                    <TextButton title={"退出"} onPress={this.props.onClose} />
                 </View>
             </HalfPanel>
         )
