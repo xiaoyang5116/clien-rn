c882bc448be14eefebd841029b27a590b6c5d9ca
KK <kk@codesky.com>
2022-07-12 10:21:54 +0800

    新增撸猫游戏

--- app/components/games/index.js
+++ app/components/games/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import RootView from "../RootView";
 import SpeedClick from "./SpeedClick";
+import TouchCat from './TouchCat';
 
 export default class Games {
 
@@ -8,6 +9,10 @@ export default class Games {
         const { id } = params;
         if (id == 1) {
             const key = RootView.add(<SpeedClick onClose={() => {
+                RootView.remove(key);
+            }} />);
+        } else if (id == 2) {
+            const key = RootView.add(<TouchCat onClose={() => {
                 RootView.remove(key);
             }} />);
         }
