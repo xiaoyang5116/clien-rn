6ce7a394d012fc8c3d9ece5065ef88ec219f657a
yx <1443584075@qq.com>
2022-07-18 19:03:21 +0800

    修复刮刮乐刮完立即关闭页面时再刮可能触发的bug

--- app/components/games/Scratch.js
+++ app/components/games/Scratch.js
@@ -14,6 +14,14 @@ import Toast from '../toast'
 import { TextButton } from '../../constants/custom-ui';
 
 class Scratch extends Component {
+    constructor(props) {
+        super(props);
+        this.timer = null
+        this.state = {
+            isScratchDone: false
+        }
+        this.timer = null
+    }
 
     onImageLoadFinished = ({ id, success }) => {
         // Do something
@@ -24,8 +32,15 @@ class Scratch extends Component {
     }
 
     onScratchDone = ({ isScratchDone, id }) => {
-        this.props.onClose()
+        this.setState({ isScratchDone: true })
         this.props.dispatch(action('SceneModel/processActions')({ __sceneId: this.props.sceneId, ...this.props }))
+        this.timer = setTimeout(() => { 
+            this.onClose()
+        }, 500)
+    }
+
+    onClose = () => {
+        this.props.onClose()
     }
 
     onScratchTouchStateChanged = ({ id, touchState }) => {
@@ -33,8 +48,8 @@ class Scratch extends Component {
         // FlatList from scrolling while scratching
         // this.setState({ scrollEnabled: !touchState });
     }
-    componentWillUnmount(){
-        this.props.onClose()
+    componentWillUnmount() {
+        clearTimeout(this.timer)
     }
 
     render() {
@@ -44,19 +59,20 @@ class Scratch extends Component {
             <HalfPanel backgroundColor={"rgba(0,0,0,0.7)"} height={"70%"}>
                 <View style={{ flex: 1, justifyContent: "space-around", alignItems: "center" }}>
                     <Text style={{ fontSize: 24, color: "#ccc" }}>刮刮乐</Text>
-                    <View style={{ width: 300, height: 300, overflow: "hidden" }}>
+                    <View style={{ width: 300, height: 300, overflow: "hidden" }} pointerEvents={this.state.isScratchDone ? "none" : "auto"}>
+                        {/* <View style={{ width: 300, height: 300, overflow: "hidden" }}> */}
                         <ScratchView
                             id={1} // ScratchView id (Optional)
                             brushSize={50} // Default is 10% of the smallest dimension (width/height)
                             threshold={45} // Report full scratch after 70 percentage, change as you see fit. Default is 50
                             fadeOut={true} // Disable the fade out animation when scratch is done. Default is true
-                            placeholderColor="#AAAAAA" // Scratch color while image is loading (or while image not present)
+                            // placeholderColor="#AAAAAA" // Scratch color while image is loading (or while image not present)
                             imageUrl={"https://s2.loli.net/2022/07/12/jr2siQNAknW3Fpz.jpg"} // A url to your image (Optional)
                             resourceName="your_image" // An image resource name (without the extension like '.png/jpg etc') in the native bundle of the app (drawble for Android, Images.xcassets in iOS) (Optional)
                             resizeMode="stretch" // Resize the image to fit or fill the scratch view. Default is stretch (cover|contain|stretch)
                             onImageLoadFinished={this.onImageLoadFinished} // 图片 loading 完成时调用
                             // onTouchStateChanged={this.onScratchTouchStateChanged} // 判断是否在刮,Touch event (to stop a containing FlatList for example)
-                            onScratchProgressChanged={this.onScratchProgressChanged} // 刮时的进度
+                            // onScratchProgressChanged={this.onScratchProgressChanged} // 刮时的进度
                             onScratchDone={this.onScratchDone} // 刮完时调用
                         />
                         <Image style={{ width: "100%", height: "100%", zIndex: -1 }} source={require("../../../assets/avatar/1.jpg")} />
