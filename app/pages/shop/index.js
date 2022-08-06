ff7a21aaf8231f1675e718eb79533d4ef5a9cafc
wsz <2974400147@qq.com>
2022-08-06 17:03:29 +0800

    道具属性更新

--- app/pages/shop/index.js
+++ app/pages/shop/index.js
@@ -222,6 +222,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#eee',
+        zIndex: 99,
     },
     viewContainer: {
         width: WIN_SIZE.width,
