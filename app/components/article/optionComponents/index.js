ed3cc70908138a91490951374c56c9b950ac3b2b
shawn <1443584075@qq.com>
2022-08-17 14:48:44 +0800

    阅读器按钮添加基本按钮

---
+++ app/components/article/optionComponents/index.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import BasicBtn from '../optionComponents/BasicBtn'
+
+const OptionComponents = (props) => {
+  const { type } = props
+  switch (type) {
+    case "basic":
+      return <BasicBtn {...props} />
+  }
+}
+
+export default OptionComponents
+
+const styles = StyleSheet.create({})
