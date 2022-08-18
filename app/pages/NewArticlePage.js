34fed3f1c366f71efd3ad6a6b45f4a4993b76e41
KK <kk@codesky.com>
2022-08-18 15:45:57 +0800

    解决：场景选项大地图Android10的BUG

--- app/pages/NewArticlePage.js
+++ app/pages/NewArticlePage.js
@@ -473,6 +473,67 @@ const TheWorld = (props) => {
   );
 }
 
+const TheWorldOther = (props) => {
+  const maskOpacity = React.useRef(new Animated.Value(1)).current;
+  const fontOpacity = React.useRef(new Animated.Value(0)).current;
+
+  const { navigation } = props;
+  const state = navigation.getState();
+  const index = state.index;
+  const activeRouteName = state.routeNames[index];
+  const routeName = props.route.name;
+
+  const onTouchTransView = () => {
+    fontOpacity.setValue(0);
+    const key = RootView.add(<WorldUnlockView {...props} onClose={() => RootView.remove(key)} />);
+  }
+
+  // 首次进入
+  React.useEffect(() => {
+    if (lo.isEqual(routeName, activeRouteName)) {
+      maskOpacity.setValue(0);
+      fontOpacity.setValue(0);
+    } else {
+      maskOpacity.setValue(1);
+      fontOpacity.setValue(0);
+    }
+  }, []);
+
+  // 通过透明度播放过度效果
+  React.useEffect(() => {
+    if (!lo.isEqual(routeName, activeRouteName)) {
+      maskOpacity.setValue(1);
+      fontOpacity.setValue(0);
+      return;
+    }
+    //
+    if (lo.isEqual(routeName, 'LeftWorld') || lo.isEqual(routeName, 'RightWorld')) {
+      Animated.sequence([
+        Animated.delay(300),
+        Animated.timing(fontOpacity, {
+          toValue: 1,
+          duration: 300,
+          useNativeDriver: false,
+        }),
+      ]).start();
+    }
+  }, [props]);
+
+  return (
+    <View style={[{ flex: 1 }, {  }]}>
+      {/* 白色遮盖层 */}
+      <Animated.View style={{ position: 'absolute', width: '100%', height: '100%', backgroundColor: '#fff', opacity: maskOpacity }} 
+        onTouchStart={onTouchTransView} 
+        pointerEvents={(lo.isEqual(routeName, 'PrimaryWorld') ? 'none' : 'auto')}>
+        <Animated.View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', opacity: fontOpacity }}>
+          {(lo.isEqual(routeName, 'LeftWorld')) ? (<Text style={styles.tranSceneFontStyle}>其实，修真可以改变现实。。。</Text>) : <></>}
+          {(lo.isEqual(routeName, 'RightWorld')) ? (<Text style={styles.tranSceneFontStyle}>所念即所现，所思即所得。。。</Text>) : <></>}
+        </Animated.View>
+      </Animated.View>
+  </View>
+  );
+}
+
 const WorldTabBar = (props) => {
   const { state, descriptors, navigation, position } = props;
   const routeKey = state.routes[state.index].key;
@@ -766,9 +827,9 @@ class NewArticlePage extends Component {
             screenOptions={{ swipeEnabled: !this.props.isStartPage }}
             sceneContainerStyle={{ backgroundColor: 'transparent' }}
             >
-            <Tab.Screen name="LeftWorld" options={{ tabBarLabel: '现实' }} children={(props) => <TheWorld {...this.props} {...props} />} />
+            <Tab.Screen name="LeftWorld" options={{ tabBarLabel: '现实' }} children={(props) => <TheWorldOther {...this.props} {...props} />} />
             <Tab.Screen name="PrimaryWorld" options={{ tabBarLabel: '尘界' }} children={(props) => <TheWorld {...this.props} {...props} />} />
-            <Tab.Screen name="RightWorld" options={{ tabBarLabel: '灵修界' }} children={(props) => <TheWorld {...this.props} {...props} />} />
+            <Tab.Screen name="RightWorld" options={{ tabBarLabel: '灵修界' }} children={(props) => <TheWorldOther {...this.props} {...props} />} />
           </Tab.Navigator>
         </View>
         <FooterContainer>
