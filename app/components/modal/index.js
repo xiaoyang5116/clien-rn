70783e717a2e009b4d45c2b72a9877014e772e8c
shawn <1443584075@qq.com>
2022-08-18 19:44:40 +0800

    dialog添加背景

--- app/components/modal/index.js
+++ app/components/modal/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import { View } from 'react-native';
 
 import RootView from '../RootView';
 import MaskModal from './MaskModal';
 import { DialogRoutes } from '../dialog';
 
+
 class Modal {
     static show(payload) {
         const { style } = payload;
         if ((style >= 6 && style <= 10) || (style === "9A" || style === "9B")) {
-            const key = RootView.add(<DialogRoutes viewData={payload} onDialogCancel={() => {
-                RootView.remove(key);
-            }} />)
+            const key = RootView.add(
+                <View style={{ flex: 1, zIndex: 1, backgroundColor: "rgba(102, 102, 102, 0.6)" }}>
+                    <DialogRoutes viewData={payload} onDialogCancel={() => {
+                        RootView.remove(key);
+                    }} />
+                </View>
+            )
         } else {
             const key = RootView.add(
                 <MaskModal data={payload} onModalHide={() => {
