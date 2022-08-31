81c0329da129c9e229f566c0ed1ee7ad65eafed9
KK <kk@codesky.com>
2022-08-31 16:06:41 +0800

    更新打坐系统UI

--- app/components/panel/Panel.js
+++ app/components/panel/Panel.js
@@ -54,8 +54,11 @@ export class Panel extends React.Component {
             <View style={{ flex: 1, backgroundColor: '#fafbfa' }} >
                 <FastImage style={{ position: 'absolute', left: 0, top: -100, width: size.width, height: 250, opacity: 0.1 }} resizeMode='cover' 
                     source={require('../../../assets/bg/panel_a.png')} />
-                <Animated.Image style={{ transform: [{rotate: spin}], position: 'absolute', left: size.width / 4, top: size.height / 4, width: '50%', height: '50%', opacity: 0.05 }} resizeMode='contain' 
-                    source={patternImage} />
+                {
+                (this.props.patternId > 0)
+                ? <Animated.Image style={{ transform: [{rotate: spin}], position: 'absolute', left: size.width / 4, top: size.height / 4, width: '50%', height: '50%', opacity: 0.05 }} resizeMode='contain' source={patternImage} />
+                : <></>
+                }
                 <FastImage style={{ position: 'absolute', left: 0, bottom: -100, width: size.width, height: 250, opacity: 0.1 }} resizeMode='cover' 
                     source={require('../../../assets/bg/panel_c.png')} />
                 {this.props.children}
