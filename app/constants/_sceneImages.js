b57d5c562fb48cede73cad345a3a09356c0e45c6
wsz <2974400147@qq.com>
2022-09-09 10:58:26 +0800

    白城任务地图

--- app/constants/_sceneImages.js
+++ app/constants/_sceneImages.js
@@ -3,6 +3,7 @@ import lo from 'lodash';
 
 const topBgImages = [
     { name: 'default', img: require('../../assets/scene/bg_default.jpg') },
+   // { name: '上部背景图57656', img: require('../../assets/scene/BGT_70789789.jpg') },
 ];
 
 // 场景背景图
