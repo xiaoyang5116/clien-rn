4de5860297edd1bc46a8f0120fb2e4278a668b1a
shawn <1443584075@qq.com>
2022-09-27 19:05:52 +0800

    添加: openUI添加供奉

--- app/pages/OpenUI.js
+++ app/pages/OpenUI.js
@@ -4,6 +4,7 @@ import XiuXingTabPage from './home/XiuXingTabPage';
 import AlchemyRoomModal from '../components/alchemyRoom';
 import { PlantPage } from '../components/plant';
 import Transitions from '../components/transition';
+import WorshipModal from '../components/worship';
 
 export default class OpenUI {
 
@@ -11,15 +12,18 @@ export default class OpenUI {
         switch (name) {
             case 'XiuXing':
                 const key = RootView.add(
-                <Transitions id={'OPEN_XIUXING_UI'}>
-                    <XiuXingTabPage onClose={() => { RootView.remove(key); }} />
-                </Transitions>);
+                    <Transitions id={'OPEN_XIUXING_UI'}>
+                        <XiuXingTabPage onClose={() => { RootView.remove(key); }} />
+                    </Transitions>);
                 break;
             case 'LianDanFang':
                 AlchemyRoomModal.show()
                 break;
             case 'ZhongZhi':
                 PlantPage.show();
+                break;
+            case 'GongFeng':
+                WorshipModal.show();
                 break;
         }
     }
