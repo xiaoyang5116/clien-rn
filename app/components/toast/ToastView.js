b11adc4997a57a64791c41d4f9fbbd25fe02e84b
shawn <1443584075@qq.com>
2022-10-12 15:34:06 +0800

    添加: Toast提示添加两个模版

--- app/components/toast/ToastView.js
+++ app/components/toast/ToastView.js
@@ -7,13 +7,17 @@ import {
     BOTTOM_TOP_SMOOTH,
     BOTTOM_TOP,
     CENTER_TOP,
-    LEFT_RIGHT
+    LEFT_RIGHT,
+    TOP_TOP_SMOOTH,
+    TOP_TOP
 } from '../../constants'
 // 导入不同的追加提示模板
 import BottomToTop from './behaviors/BottomToTop';
 import LeftToRight from './behaviors/LeftToRight';
 import BottomToTopSmooth from './behaviors/BottomToTopSmooth';
 import CenterToTop from './behaviors/CenterToTop';
+import TopToTopSmooth from './behaviors/TopToTopSmooth';
+import TopToTop from './behaviors/TopToTop';
 
 
 class ToastView extends Component {
@@ -34,6 +38,14 @@ class ToastView extends Component {
             case CENTER_TOP:
                 return (
                     <CenterToTop {...this.props} onHide={this.onHide} />
+                )
+            case TOP_TOP_SMOOTH:
+                return (
+                    <TopToTopSmooth {...this.props} onHide={this.onHide} />
+                )
+            case TOP_TOP:
+                return (
+                    <TopToTop {...this.props} onHide={this.onHide} />
                 )
             case LEFT_RIGHT:
                 return (
