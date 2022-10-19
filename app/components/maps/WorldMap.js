74a1e48d797aeb9c1ceda862d34e41ecf1476af4
KK <kk@codesky.com>
2022-10-19 11:49:43 +0800

    优化：世界地图

--- app/components/maps/WorldMap.js
+++ app/components/maps/WorldMap.js
@@ -14,6 +14,7 @@ import {
 import lo from 'lodash';
 import FastImage from 'react-native-fast-image';
 
+import { getWindowSize } from '../../constants';
 import { getWindowHeight, getWindowWidth, px2pd } from '../../constants/resolution';
 import { MAP_DATA } from './data/WorldMapData_1';
 
@@ -47,94 +48,160 @@ const generateGridBound = (gridId) => {
 }
 
 const WorldMap = (props) => {
-  // 地图起始点坐标
+  // 出生点坐标
   const mapInitXY = { 
     x: ((getWindowWidth() / 2) - MAP_GRID_WIDTH / 2), 
     y: ((getWindowHeight() / 2) - MAP_GRID_HEIGHT / 2)
   };
 
   // 地图位移
-  const bigMapPos = React.useRef(new Animated.ValueXY(mapInitXY)).current;
+  const mapPos = React.useRef(new Animated.ValueXY(mapInitXY)).current;
 
   // 瓦片列表
   const [grids, setGrids] = React.useState([]);
 
-  // 各种状态数  
-  const status = React.useRef({ prevX: 0, prevY: 0 }).current;
+  // 各种状态数
+  const status = React.useRef({ prevX: 0, prevY: 0, lastX: 0, lastY: 0 }).current;
 
-  // 地图滑动处理器
+  // 记录触摸状态
+  const isTouchStart = React.useRef(false);
+
+  // 是否在移动
+  const isMoving = React.useRef(false);
+
+  // 地图坐标发生改变
+  const onMapPositionChanged = ({ x, y }) => {
+    if (status.lastX == x && status.lastY == y)
+      return;
+
+    status.lastX = x;
+    status.lastY = y;
+
+    const diffX = mapInitXY.x - x; // 距离出生点偏移量X
+    const diffY = mapInitXY.y - y; // 距离出生点偏移量Y
+    const diffColumns = Math.floor(diffX / MAP_GRID_WIDTH); // 距离出生点偏移列数
+    const diffRows = Math.floor(diffY / MAP_GRID_HEIGHT); // 距离出生点偏离行数
+    const gridId = (MAP_COLUMNS * ((MAP_ROWS / 2) + (diffRows - 1))) + ((MAP_COLUMNS / 2) + diffColumns); // 中心点瓦片ID
+
+    setGrids((list) => {
+      const boundGridIds = generateGridBound(gridId);
+      const newGrids = [];
+
+      lo.forEach(boundGridIds, (item) => {
+        const found = lo.find(list, (v) => (parseInt(v.key) == item.gridId));
+        if (found == undefined) {
+          const grid = (<Grid key={item.gridId} gridId={item.gridId} style={[
+            { transform: [{ translateX: mapPos.x }, { translateY: mapPos.y }] },
+            { left: ((diffColumns - item.x) * MAP_GRID_WIDTH), top: ((diffRows - item.y) * MAP_GRID_HEIGHT) }
+          ]} />);
+          newGrids.push(grid);
+        } else {
+          newGrids.push(found);
+        }
+      });
+
+      if (newGrids.length <= 0) {
+        return list; // 没发生变化，直接返回。
+      }
+
+      return [...newGrids];
+    });
+  }
+
+  // moveTo 完毕后执行
+  const onMoved = () => {
+    status.prevX = mapPos.x._value;
+    status.prevY = mapPos.y._value;
+  }
+
+  // dx,dy: 从按下后移动距离
+  // duration: 大于0则动画移动
+  const moveTo = (dx, dy, duration = 0) => {
+    const x = status.prevX + dx;
+    const y = status.prevY + dy;
+
+    if (duration > 0) {
+      Animated.timing(mapPos, {
+        toValue: { x, y },
+        duration: duration,
+        useNativeDriver: true,
+      }).start(({ finished }) => {
+        if (finished) {
+          onMoved();
+        }
+      });
+    } else {
+      mapPos.setValue({ x, y });
+    }
+  }
+
+  // 拖拽处理器
   const panResponder = React.useRef(PanResponder.create({
     onMoveShouldSetPanResponder: () => true,
     onPanResponderGrant: (evt, gestureState) => {
-      status.prevX = bigMapPos.x._value;
-      status.prevY = bigMapPos.y._value;
     },
     onPanResponderMove: (evt, gestureState) => {
       // 单指操作
       if (gestureState.numberActiveTouches === 1) {
-        const x = status.prevX + gestureState.dx;
-        const y = status.prevY + gestureState.dy;
-        bigMapPos.setValue({ x, y });
-
-        const diffX = mapInitXY.x - x; // 距离出生点偏移量X
-        const diffY = mapInitXY.y - y; // 距离出生点偏移量Y
-        const diffColumns = Math.floor(diffX / MAP_GRID_WIDTH); // 距离出生点偏移列数
-        const diffRows = Math.floor(diffY / MAP_GRID_HEIGHT); // 距离出生点偏离行数
-        const gridId = (MAP_COLUMNS * ((MAP_ROWS / 2) + (diffRows - 1))) + ((MAP_COLUMNS / 2) + diffColumns); // 中心点瓦片ID
-
-        setGrids((list) => {
-          const boundGridIds = generateGridBound(gridId);
-          const newGrids = [];
-
-          lo.forEach(boundGridIds, (item) => {
-            const found = lo.find(list, (v) => (parseInt(v.key) == item.gridId));
-            if (found == undefined) {
-              const grid = (<Grid key={item.gridId} gridId={item.gridId} style={[
-                { transform: [{ translateX: bigMapPos.x }, { translateY: bigMapPos.y }] },
-                { left: ((diffColumns - item.x) * MAP_GRID_WIDTH), top: ((diffRows - item.y) * MAP_GRID_HEIGHT) }
-              ]} />);
-              newGrids.push(grid);
-            } else {
-              newGrids.push(found);
-            }
-          });
-
-          if (newGrids.length <= 0) {
-            return list; // 没发生变化，直接返回。
-          }
-
-          return [...newGrids];
-        });
+        moveTo(gestureState.dx, gestureState.dy);
       }
+      // 中断触摸点击
+      isTouchStart.current = false;
     },
-
     onPanResponderRelease: (evt, gestureState) => {
-      status.prevX = bigMapPos.x._value;
-      status.prevY = bigMapPos.y._value;
+      onMoved();
     },
   })).current;
 
+  const touchStartHandler = () => {
+    isTouchStart.current = true;
+  }
+
+  const touchEndHandler = (e) => {
+    // 如果触摸被手势中断，优先处理手势，忽略触摸点击。
+    if (!isTouchStart.current)
+      return;
+
+    const { pageX, pageY } = e.nativeEvent;
+    const winSize = getWindowSize();
+    const dx = (winSize.width / 2) - pageX;
+    const dy = (winSize.height / 2) - pageY;
+
+    moveTo(dx, dy, 300);
+    isTouchStart.current = false;
+  }
+
+  React.useEffect(() => {
+    const key = mapPos.addListener(onMapPositionChanged);
+
+    moveTo(0, 0);
+
+    return () => {
+      mapPos.removeListener(key);
+    }
+  }, []);
+
   return (
-  <View style={[{ flex: 1, backgroundColor: 'rgba(0,0,0,0.5)' }, { borderWidth: 2, borderColor: '#669900' }]} {...panResponder.panHandlers}>
-    {/* 瓦片集合 */}
-    <View style={{ flex: 1 }}>
-      {grids}
-    </View>
+    <View style={{ flex: 1, backgroundColor: 'rgba(0,0,0,0.5)' }} {...panResponder.panHandlers} onTouchStart={touchStartHandler} onTouchEnd={touchEndHandler}>
+      {/* 瓦片集合 */}
+      <View style={{ flex: 1 }}>
+        {grids}
+      </View>
 
-    {/* 角色 */}
-    <View style={{ position: 'absolute', width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center' }}>
-      <FastImage source={require('../../../assets/bg/explore_person.png')} style={{ width: px2pd(185), height: px2pd(166) }} />
-    </View>
+      {/* 角色 */}
+      <View style={{ position: 'absolute', width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center' }}>
+        <FastImage source={require('../../../assets/bg/explore_person.png')} style={{ width: px2pd(185), height: px2pd(166) }} />
+      </View>
 
-    {/* 大地图关闭按钮 */}
-    <View style={{ position: 'absolute', right: px2pd(20), top: 50 }} onTouchStart={() => {
-      if (props.onClose != undefined) {
-        props.onClose();
-      }
-    }}>
-      <FastImage style={{ width: px2pd(130), height: px2pd(56) }} source={require('../../../assets/button/map_close_button.png')} />
+      {/* 大地图关闭按钮 */}
+      <View style={{ position: 'absolute', right: px2pd(20), top: 50 }} onTouchStart={() => {
+        if (props.onClose != undefined) {
+          props.onClose();
+        }
+      }}>
+        <FastImage style={{ width: px2pd(130), height: px2pd(56) }} source={require('../../../assets/button/map_close_button.png')} />
+      </View>
     </View>
-  </View>
   );
 }
 
