e33c5c19db808d827cadf35fb47a839f54dd5247
KK <kk@codesky.com>
2022-10-27 15:43:20 +0800

    UPDATE

--- app/pages/home/ArenaPage.js
+++ app/pages/home/ArenaPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 
 import {
     action,
@@ -16,6 +16,7 @@ import FastImage from 'react-native-fast-image';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import { SafeAreaView } from 'react-native';
 import { px2pd } from '../../constants/resolution';
+import { useImperativeHandle } from 'react';
 
 const MsgItem = (props) => {
     return (
@@ -25,7 +26,22 @@ const MsgItem = (props) => {
     )
 }
 
-const Character = (props) => {
+const Character = (props, ref) => {
+    const [lifePercent, setLifePercent] = React.useState(0);
+
+    useImperativeHandle(ref, () => ({
+        update: (data) => {
+            // 更新角色血条
+            if (data.attackerUid != undefined && data.defenderUid != undefined) {
+                if (props.user.uid == data.attackerUid) {
+                    setLifePercent((data.attackerLife / data.attackerOrgLife) * 100);
+                } else {
+                    setLifePercent((data.defenderLife / data.defenderOrgLife) * 100);
+                }
+            }
+        },
+    }));
+
     return (
     <View style={[{ flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', height: 100, backgroundColor: '#403340' }, (props.contentStyle != undefined) ? props.contentStyle : {}]}>
         <View style={{ width: 90, height: 90, marginLeft: 5, marginRight: 5, flexDirection: 'row', borderRadius: 10, justifyContent: 'center', alignItems: 'center',  }}>
@@ -34,11 +50,10 @@ const Character = (props) => {
         </View>
         <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center', height: '100%' }}>
             <View style={{ height: 3, marginTop: 6, marginRight: 6, marginBottom: 0 }}>
-                <ProgressBar percent={20} />
+                <ProgressBar percent={50} sections={[{x: 0, y: 100, color: '#3390ff'}]} />
             </View>
             <View style={{ height: 12, marginTop: 0, marginRight: 6, marginBottom: 3 }}>
-                <ProgressBar percent={80} />
-                {/* <ProgressBar percent={enemyLifePercent} /> */}
+                <ProgressBar percent={lifePercent} />
             </View>
             <View style={{ height: 15, marginTop: 6, marginRight: 6, marginBottom: 6 }}>
                 <ProgressBar percent={props.user.power / 1000 * 100} sections={[{x: 0, y: 100, color: '#12b7b5'}]} />
@@ -57,11 +72,14 @@ const Character = (props) => {
     </View>
     );
 }
+const CharacterWrapper = forwardRef(Character);
 
 const ArenaPage = (props) => {
     const refList = React.createRef();
     const listData = React.useRef([]);
     const reportIndex = React.useRef(0);
+    const refCharacterEnemy = React.useRef(null);
+    const refCharacterMysef = React.useRef(null);
     const [update, setUpdate] = React.useState({});
 
     const renderMsgItem = (data) => {
@@ -73,32 +91,34 @@ const ArenaPage = (props) => {
     }, []);
 
     React.useEffect(() => {
+        let timer = null;
         if (props.report.length > 0) {
             if (reportIndex.current < props.report.length) {
-                setTimeout(() => {
+                timer = setTimeout(() => {
                     setUpdate({});
                 }, 600);
             } else {
-                listData.current.length = 0;
+                // listData.current.length = 0;
                 reportIndex.current = 0;
                 props.dispatch(action('ArenaModel/over')());
             }
         }
+        return () => {
+            if (timer != null) {
+                clearTimeout(timer);
+            }
+        }
     }, [props.report, update]);
 
-    let myLifePercent = 0;
-    let enemyLifePercent = 0;
     if (props.report.length > 0 && reportIndex.current < props.report.length) {
         const item = props.report[reportIndex.current];
         listData.current.push({ id: listData.current.length + 1, msg: '<li style="color: #ffffff">{0}</li>'.format(item.msg) });
 
-        if (props.myself.uid == item.attackerUid) {
-            myLifePercent = (item.attackerLife / item.attackerOrgLife) * 100;
-            enemyLifePercent = (item.defenderLife / item.defenderOrgLife) * 100;
-        } else {
-            myLifePercent = (item.defenderLife / item.defenderOrgLife) * 100;
-            enemyLifePercent = (item.attackerLife / item.attackerOrgLife) * 100;
-        }
+        setTimeout(() => {
+            refCharacterEnemy.current.update(item);
+            refCharacterMysef.current.update(item);
+        }, 0);
+
         reportIndex.current += 1;
     }
 
@@ -112,24 +132,21 @@ const ArenaPage = (props) => {
                         }
                     }} />
                 </View>
-                <Character user={props.enemy} contentStyle={{ marginTop: px2pd(80) }} />
+                <CharacterWrapper ref={refCharacterEnemy} user={props.enemy} contentStyle={{ marginTop: px2pd(80) }} />
                 <View style={{ flex: 1, flexDirection: 'row', backgroundColor: '#5a5a70' }}>
                     <FlatList
                         ref={refList}
                         data={listData.current}
                         renderItem={renderMsgItem}
                         keyExtractor={item => item.id}
-                        getItemLayout={(_data, index) => (
-                            {length: 20, offset: 20 * index, index}
-                        )}
                         onContentSizeChange={() => {
                             if (listData.current.length > 0) {
-                                refList.current.scrollToIndex({ index: listData.current.length - 1 });
+                                refList.current.scrollToEnd({ animated: true });
                             }
                         }}
                     />
                 </View>
-                <Character user={props.myself} />
+                <CharacterWrapper ref={refCharacterMysef} user={props.myself} />
             </View>
         </SafeAreaView>
     );
