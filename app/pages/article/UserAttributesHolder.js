70de1bb9e8089e0119a87f7e8cd9a1df0d8ac583
shawn <1443584075@qq.com>
2022-11-01 15:06:10 +0800

    修改：角色属性页面UI

--- app/pages/article/UserAttributesHolder.js
+++ app/pages/article/UserAttributesHolder.js
@@ -3,7 +3,8 @@ import {
   View,
   Text,
   DeviceEventEmitter,
-  SafeAreaView
+  SafeAreaView,
+  Image
 } from 'react-native'
 import React from 'react'
 
@@ -15,6 +16,8 @@ import {
 
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Collapse from '../../components/collapse';
+import FastImage from 'react-native-fast-image';
+import { px2pd } from '../../constants/resolution';
 
 const UserAttributesHolder = (props) => {
   const [data, setData] = React.useState(props.config);
@@ -50,15 +53,21 @@ const UserAttributesHolder = (props) => {
   }, []);
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 1 }}>
-        <AntDesign name='left' size={23} style={{ margin: 5 }} />
+    <SafeAreaView style={{ flex: 1, }}>
+      <Image style={{ width: px2pd(1080), height: px2pd(2400), position: 'absolute', }} source={require('../../../assets/bg/roleAttribute_bg.png')} />
+      <View style={{ flex: 1, }}>
+        <View>
+          <FastImage style={{ width: px2pd(1132), height: px2pd(107), position: 'absolute' }} source={require('../../../assets/bg/roleAttribute_back_bg.png')} />
+          <AntDesign name='left' size={23} style={{ margin: 5 }} />
+        </View>
+
         <View style={{ flex: 1 }} onTouchStart={(e) => e.stopPropagation()}>
           <Collapse
             data={data}
             renderItem={(item) => {
               return (
                 <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                  <FastImage style={{ width: "100%", height: "100%", position: 'absolute' }} source={require('../../../assets/bg/roleAttribute_bg2.png')} />
                   <View style={{ width: 50 }}><Text>{item.title}:</Text></View>
                   <View><Text style={{ color: '#666' }}>{item.value}</Text></View>
                 </View>
