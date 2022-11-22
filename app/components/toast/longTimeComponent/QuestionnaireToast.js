b92addc9280a571cca2f5c01adfb92262be59d15
shawn <1443584075@qq.com>
2022-11-22 17:15:15 +0800

    修改：调查问卷属性提示时间改为2秒

--- app/components/toast/longTimeComponent/QuestionnaireToast.js
+++ app/components/toast/longTimeComponent/QuestionnaireToast.js
@@ -35,7 +35,7 @@ const LeftToRightSwiper = props => {
       Animated.timing(opacityAnim, {
         toValue: 0,
         duration: 2000,
-        delay: 3000,
+        delay: 2000,
         useNativeDriver: false,
       }),
     ]).start(onHide);
