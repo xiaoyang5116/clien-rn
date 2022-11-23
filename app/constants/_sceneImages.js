e08a80a1430547f99a1c79149122750975d62bcd
wsz <2974400147@qq.com>
2022-11-23 18:27:50 +0800

    2022年11月23日

--- app/constants/_sceneImages.js
+++ app/constants/_sceneImages.js
@@ -106,6 +106,7 @@ const topBgImages = [
     { name: 'BGT_102', img: require('../../assets/scene/BGT_102.webp') },
     { name: 'BGT_103', img: require('../../assets/scene/BGT_103.webp') },
     { name: 'BGT_104', img: require('../../assets/scene/BGT_104.webp') },
+    { name: 'BGT_105', img: require('../../assets/scene/BGT_105.webp') },
 
 
     
