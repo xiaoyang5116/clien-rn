b345199e06a01cd6e16e40f65e410c61e9fb81ed
shawn <1443584075@qq.com>
2022-12-06 14:11:29 +0800

    修改：中文属性对应英文属性

--- app/utils/AttributeUtils.js
+++ app/utils/AttributeUtils.js
@@ -23,6 +23,20 @@ export function getAttributeChineseName(enName) {
 
 // 查找中文属性对应的英文名称
 export function getAttributeEnglishName(cnName) {
-    const found = lo.find(ATTRIBUTE_MAP, (e) => lo.isEqual(e[0], cnName));
+    const found = lo.find(ATTRIBUTE_MAP_2, (e) => lo.isEqual(e[0], cnName));
     return (found != undefined) ? found[1] : undefined;
 }
+
+const ATTRIBUTE_MAP_2 = [
+    ['伤害',    'damage'],
+    ['体力',    'hp'],
+    ['法力',    'mp'],
+    ['速度',    'speed'],
+    ['攻击',    'attack'],
+    ['防御',    'defense'],
+    ['物理攻击', 'physicalAttack'],
+    ['法术攻击', 'magicAttack'],
+    ['物理防御', 'physicalDefense'],
+    ['法术防御', 'magicDefense'],
+];
+
