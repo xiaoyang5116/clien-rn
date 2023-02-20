9c094222e196e5ba806ab45eac5f8795f108e2b8
shawn <1443584075@qq.com>
2023-02-20 17:59:49 +0800

    修改: 上下标题按钮UI

--- app/components/article/optionComponents/SingleLine.js
+++ app/components/article/optionComponents/SingleLine.js
@@ -6,6 +6,19 @@ import { ImageButton, BtnIcon, ImageBtn } from '../../../constants/custom-ui';
 import { px2pd } from '../../../constants/resolution';
 
 
+const imgData = {
+  center: {
+    size: { width: px2pd(1070), height: px2pd(98), },
+    default: require('../../../../assets/button/center_titleButton/default.png'),
+    default_click: require('../../../../assets/button/center_titleButton/default_click.png'),
+    // disabled
+    disabled: require('../../../../assets/button/center_titleButton/disabled.png'),
+    disabled_click: require('../../../../assets/button/center_titleButton/disabled_click.png'),
+  },
+}
+
+
+
 const SingleLine = (props) => {
   const { title, disabled, onPress, currentStyles, optionData, containerStyle } = props;
   const { icon } = optionData;
@@ -18,29 +31,13 @@ const SingleLine = (props) => {
     }
   }
 
-  if (disabled) {
-    return (
-      <View style={{ width: "100%", alignItems: "center" }}>
-        <ImageBtn
-          imgStyle={{ width: px2pd(1067), height: px2pd(98), }}
-          onPress={handlerOnPress}
-          source={require('../../../../assets/button/topbot_btn_2/content1_2.png')}
-          selectedSource={require('../../../../assets/button/topbot_btn_2/content2_2.png')}
-        >
-          {icon?.show ? <BtnIcon id={icon.id} style={{ height: "100%", justifyContent: "center" }} /> : null}
-          <Text style={{ fontSize: 14, color: "#000" }}>{title}</Text>
-        </ImageBtn>
-      </View>
-    )
-  }
-
   return (
     <View style={{ width: "100%", alignItems: "center" }}>
       <ImageBtn
-        imgStyle={{ width: px2pd(1067), height: px2pd(98), }}
+        imgStyle={imgData.center.size}
         onPress={handlerOnPress}
-        source={require('../../../../assets/button/topbot_btn/content1.png')}
-        selectedSource={require('../../../../assets/button/topbot_btn/content2.png')}
+        source={disabled ? imgData.center.disabled : imgData.center.default}
+        selectedSource={disabled ? imgData.center.disabled_click : imgData.center.default_click}
       >
         {icon?.show ? <BtnIcon id={icon.id} style={{ height: "100%", justifyContent: "center" }} /> : null}
         <Text style={{ fontSize: 14, color: "#000" }}>{title}</Text>
